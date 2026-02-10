effc112cbfed9b33c2e649c03f5c3b71c5b87932
marcelin <marcelin2509@gmail.com>
2026-02-10 13:29:05 -0500

    twosum final

---
+++ twosum/main.py
@@ -0,0 +1,29 @@
+def twosum(nums: list[int], target: int) -> list[int]:
+   
+# brute force approach
+
+#    for i in range(len(nums)):
+#        for j in range(i + 1, len(nums)):
+           
+#            if nums[i] + nums[j] == target:
+#                return [i, j]
+   
+#    return []
+
+# hash map approach
+
+    num_map = {}
+    for i, num in enumerate(nums):
+        
+        complement = target - num
+        if complement in num_map:
+            return [num_map[complement], i]
+        num_map[num] = i
+    
+    return []    
+
+print("enter the list of numbers separated by spaces:")
+nums = list(map(int, input().split()))
+print("enter the target sum:")
+target = int(input())
+print("Indices of the two numbers that add up to the target are:", twosum(nums, target))
