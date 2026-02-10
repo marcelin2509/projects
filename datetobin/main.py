849a6a68b426a088e357dd6e284f51c00bc37e2c
marcelin2509 <marcelin2509@gmail.com>
2026-02-10 16:01:48 -0500

    datetobin final

--- datetobin/main.py
+++ datetobin/main.py
@@ -4,7 +4,7 @@ def datetobin(date: str) -> str:
     year_bin = format(int(year), '016b')  # 16 bits for the year
     month_bin = format(int(month), '08b')  # 8 bits for the month
     day_bin = format(int(day), '08b')      # 8 bits for the day
-    return year_bin + month_bin + day_bin
+    return f"{year_bin}-{month_bin}-{day_bin}"
 
 print("Enter a date in the format 'YYYY-MM-DD':")
 date_input = str(input())
